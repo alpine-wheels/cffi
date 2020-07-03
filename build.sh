76e9d86c52e65803cd3dd01e5524b0da5e8d6c3d
William Jackson <william@subtlecoolness.com>
2020-07-03 11:23:54 -0500

    Simplify build script

--- build.sh
+++ build.sh
@@ -3,15 +3,7 @@
 set -e
 
 # install os dependencies
-apk add --no-cache gcc libffi-dev mercurial musl-dev
-
-# find version
-CFFI_VERSION="$(awk -F '==' '/cffi/ { print $2 }' requirements.txt)"
-
-# check out source code
-rm -rf cffi
-hg clone --update "v${CFFI_VERSION}" https://foss.heptapod.net/pypy/cffi
+apk add --no-cache gcc libffi-dev musl-dev
 
 # build
-cd cffi || exit
-python setup.py bdist_wheel
+pip wheel --no-deps --requirement requirements.txt
