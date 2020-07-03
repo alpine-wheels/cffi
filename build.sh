ee44ec6173cb4aa7734f0ad5ad01c760ab68f813
William Jackson <william@subtlecoolness.com>
2020-07-03 08:06:09 -0500

    Initial commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+# install os dependencies
+apk add --no-cache gcc libffi-dev mercurial musl-dev
+
+# find version
+CFFI_VERSION="$(awk -F '==' '/cffi/ { print $2 }' requirements.txt)"
+
+# check out source code
+rm -rf cffi
+hg clone --update "v${CFFI_VERSION}" https://foss.heptapod.net/pypy/cffi
+#hg update "v${CFFI_VERSION}"
+
+# build
+cd cffi || exit
+python setup.py bdist_wheel
